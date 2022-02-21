5ec59eaaffb775879487e7a4b51e8d1fc6d9cbb7
tuguzT <timurka.tugushev@gmail.com>
2022-02-21 23:16:56 +0300

    Make camera rotate around the plane

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,8 +6,8 @@ public class CameraController : MonoBehaviour
     private Camera currentCamera;
 
     [SerializeField]
-    [Range(0, 100)]
-    private float cameraSpeed = 50;
+    [Range(0, 200)]
+    private float cameraSpeed = 100;
 
     [SerializeField]
     [Range(0, 90)]
@@ -22,19 +22,31 @@ public class CameraController : MonoBehaviour
     private float distance = 10;
 
     private Vector3 _center;
+    private float _rotationX;
+    private float _rotationY;
 
     private void Start()
     {
         var myTransform = transform;
         _center = myTransform.position;
 
-        currentCamera.transform.position = _center + Vector3.forward * distance;
-        currentCamera.transform.RotateAround(_center, Vector3.left, minimumAngle);
+        currentCamera.transform.position = _center + myTransform.forward * distance;
+        var angle = (minimumAngle + maximumAngle) / 2;
+        currentCamera.transform.RotateAround(_center, -myTransform.right, angle);
         currentCamera.transform.LookAt(_center);
     }
 
     private void Update()
     {
-        currentCamera.transform.RotateAround(_center, Vector3.up, cameraSpeed * Time.deltaTime);
+        var mouseX = Input.GetAxis("Mouse X");
+        var mouseY = Input.GetAxis("Mouse Y");
+
+        _rotationY += mouseX * cameraSpeed * Time.deltaTime;
+        _rotationX += mouseY * cameraSpeed * Time.deltaTime;
+        _rotationX = Mathf.Clamp(_rotationX, minimumAngle, maximumAngle);
+
+        var cameraTransform = currentCamera.transform;
+        cameraTransform.localEulerAngles = new Vector3(_rotationX, _rotationY);
+        cameraTransform.position = _center - cameraTransform.forward * distance;
     }
 }
