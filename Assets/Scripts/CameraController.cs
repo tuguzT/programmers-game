5b52034f6d221475f6cc5fee91f3dcb589ef279f
tuguzT <timurka.tugushev@gmail.com>
2022-04-05 18:14:42 +0300

    Add camera zoom

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using Model;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
@@ -10,27 +11,37 @@ public class CameraController : MonoBehaviour
     private float cameraSpeed = 100;
 
     [SerializeField]
-    [Range(0, 90)]
+    [Range(0, 20)]
+    private float zoomSpeed = 5;
+
+    [SerializeField]
+    [Range(0, 40)]
     private float minimumAngle = 30;
 
     [SerializeField]
-    [Range(0, 90)]
+    [Range(50, 90)]
     private float maximumAngle = 60;
-
-    [SerializeField]
-    [Range(0, 100)]
-    private float distance = 10;
 
     private Vector3 _center;
     private float _rotationX;
     private float _rotationY;
+
+    private float _zoomDistance;
+    private float _distance;
+    private float _minDistance;
+    private float _maxDistance;
 
     private void Start()
     {
         var myTransform = transform;
         _center = myTransform.position;
 
-        currentCamera.transform.position = _center + myTransform.forward * distance;
+        var fieldWidth = GameManager.Instance.GameMode.FieldWidth();
+        _minDistance = fieldWidth * ChunkData.Width;
+        _maxDistance = fieldWidth * 1.5f * ChunkData.Width;
+        _zoomDistance = _distance = (_minDistance + _maxDistance) / 2;
+
+        currentCamera.transform.position = _center + myTransform.forward * _distance;
         var angle = (minimumAngle + maximumAngle) / 2;
         currentCamera.transform.RotateAround(_center, -myTransform.right, angle);
         currentCamera.transform.LookAt(_center);
@@ -47,6 +58,10 @@ public class CameraController : MonoBehaviour
 
         var cameraTransform = currentCamera.transform;
         cameraTransform.localEulerAngles = new Vector3(_rotationX, _rotationY);
-        cameraTransform.position = _center - cameraTransform.forward * distance;
+
+        _zoomDistance -= Input.mouseScrollDelta.y * 0.25f;
+        _zoomDistance = Mathf.Clamp(_zoomDistance, _minDistance, _maxDistance);
+        _distance = Mathf.MoveTowards(_distance, _zoomDistance, zoomSpeed * Time.deltaTime);
+        cameraTransform.position = _center - cameraTransform.forward * _distance;
     }
 }
